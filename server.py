452f65c362d3f2b901c0895000720cbd39d968aa
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-08-28 21:04:27 +0530

    Update server.py

--- server.py
+++ server.py
@@ -32,8 +32,8 @@ async def handle_message(client, message):
     user=message.from_user
     user=user.id
     username=user.username
+    asyncio.ensure_future(timeout_message(message.chat.id))
     profile = app.send_message(message.chat.id, text="**State your profile link.**"
-        asyncio.ensure_future(timeout_message(message.chat.id))
     app.on_message(filters.private & filters.text)
     async def get_punishment_date(bot, message):    
         pun = app.send_message(message.chat.id, text="**Mention the date of your punishment.**"
