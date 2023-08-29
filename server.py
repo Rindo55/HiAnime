f6eebf19503265d4bbc952e09a8f6671681348b2
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-08-29 23:27:48 +0530

    Update server.py

--- server.py
+++ server.py
@@ -37,7 +37,9 @@ post_type = "text"
 text = "Hello, world!"
 
 # create the post using the Comments API
-uploadxz = requests.post(url="https://api.comments.bot/creatPost", api_key=apiz_key, owner_id=owner_id, type=post_type, text=text).json()
+x_url="https://api.comments.bot/createPost"
+tgshare = requests.get(x_url, params={"api_url": apiz_url, "owner_id": owner_id, "type": post_type, "text": text})
+uploadxz=tgshare.json()
 directlink = uploadxz["result"]
 # print the result
 print(directlink)
