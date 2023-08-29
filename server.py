5eb933532cca2a78bc3fdea62293f104234bbfc2
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-08-29 20:17:30 +0530

    Update server.py

--- server.py
+++ server.py
@@ -28,46 +28,24 @@ async def timeoutz_message(chat_id):
     await app.send_message(chat_id, "Beep Boop! 5 minutes up, no response has been received. Your appeal has timed out. If you'd like to submit the appeal, please click [here](https://t.me/aniwatchappealbot?start=appeal).")
 user_states = {}
 user_messages = {}
-@app.on_callback_query(filters.regex("vote"))
-async def votes_(_,query: CallbackQuery):
-        id = query.message.message_id
-        user = query.from_user.id
-        vote = int(query.data.replace("vote","").strip())
-
-        is_vote = await is_voted(id,user)
-        if is_vote == 1:
-            return await query.answer("You Have Already Voted... You Can't Vote Again")
-        await query.answer()
-
-        x = query.message.reply_markup['inline_keyboard'][0]
-        a = x[0]['text'].replace('👍','').strip()
-        b = x[1]['text'].replace('♥️','').strip()
-        c = x[2]['text'].replace('👎','').strip()
-
-        if a == "":
-            a = 0
-        if b == "":
-            b = 0
-        if c == "":
-            c = 0
-        
-        a = int(a)
-        b = int(b)
-        c = int(c)
-
-        if vote == 1:
-            a = a + 1
-            buttons = get_vote_buttons(a,b,c)
-            await query.message.edit_reply_markup(reply_markup=buttons)
-        elif vote == 2:
-            b = b + 1
-            buttons = get_vote_buttons(a,b,c)
-            await query.message.edit_reply_markup(reply_markup=buttons)
-        elif vote == 3:
-            c = c + 1
-            buttons = get_vote_buttons(a,b,c)
-            await query.message.edit_reply_markup(reply_markup=buttons)
-        return
+def gen_markup():
+    markup = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton("ACCEPT", callback_data="cb_yes"),
+                InlineKeyboardButton("DENY", callback_data="cb_no")
+            ]
+        ]
+    )
+    return markup
+async def handle_callback_query(client, query):
+    global yes_votes, no_votes
+    if query.data == "cb_yes":
+        yes_votes += 1
+        await query.answer("You accepted this appeal")
+    elif query.data == "cb_no":
+        no_votes += 1
+        await query.answer("You declined this voted")
 
 # Define a function to handle the /start command
 @app.on_message(filters.regex('/start appeal'))
@@ -80,6 +58,7 @@ def start(bot, update):
 # Define a function to handle user messages
 @app.on_message(filters.text)
 async def handle_message(bot, update):
+    global yes_votes, no_votes
     user_id = update.from_user.id
     if user_id in user_states:
         state = user_states[user_id]
@@ -107,15 +86,7 @@ async def handle_message(bot, update):
                 await app.send_message(user_id, "Your appeal has been received and is now under review.")
                 combined_message = "\n".join(user_messages[user_id])  # Combine user messages
                 ch_id = -1001582654217
-                await app.send_message(ch_id, text="User ID: {user_id}\n\n{combined_message}", buttons = InlineKeyboardMarkup(
-                    [
-                        [
-                            InlineKeyboardButton(text=f"👍 {a}", callback_data="vote1"),
-                            InlineKeyboardButton(text=f"♥️ {b}", callback_data="vote2"),
-                            InlineKeyboardButton(text=f"👎 {c}", callback_data="vote3")
-                        ]
-                    ]
-                ))
+                await app.send_message(ch_id, text="User ID: {user_id}\n\n{combined_message}", reply_markup=gen_markup())
                 del user_states[user_id]
                 del user_messages[user_id]
             else:
