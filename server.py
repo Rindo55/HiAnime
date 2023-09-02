9195014e8a22149236c5ac34c9403fe6906bbb58
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-09-02 17:55:51 +0530

    Update server.py

--- server.py
+++ server.py
@@ -69,8 +69,9 @@ async def start(bot, cmd: Message):
         user_id = cmd.from_user.id
         user_states[user_id] = 'send_link'
         xc_id=int(usr_cmd.split("_")[-1])
-        ment = app.get_users(xc_id).mention()
-        await app.send_message(user_id, f"**Send me the message you want me to forward to** {ment}")
+        userxd = app.get_users(xc_id)
+        usrc = userxd.mention()
+        await app.send_message(user_id, f"**Send me the message you want me to forward to** {usrc}")
         
     elif usr_cmd == "/start appeal":
         user_id = cmd.from_user.id
