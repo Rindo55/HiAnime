c9a6f885ebd380ecf3a36aa4a9ac912f064c1afd
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-09-02 15:16:26 +0530

    Update server.py

--- server.py
+++ server.py
@@ -77,15 +77,16 @@ VOTE_MARKUP = InlineKeyboardMarkup(
 
 # Define a function to handle the /start command
 @app.on_message(filters.command("start") & filters.private)
-async def start(bot, cmd: Message, update):
+async def start(bot, cmd: Message):
     usr_cmd = cmd.text.split("_", 1)[-1]
     if usr_cmd == "/start":
        await cmd.reply_text("Bot seems online! ⚡️")
     else:
-        user_id = update.from_user.id
-        user_states[user_id] = 'waiting_link'
-        user_messages[user_id] = []  # Initialize an empty list for user messages
-        await app.send_message(user_id, "State your **AniWatch profile link.**")
+        def startx(bot, update):
+            user_id = update.from_user.id
+            user_states[user_id] = 'waiting_link'
+            user_messages[user_id] = []  # Initialize an empty list for user messages
+            await app.send_message(user_id, "State your **AniWatch profile link.**")
 
 # Define a function to handle user messages
 @app.on_message(filters.text)
